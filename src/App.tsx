f17e872d3dde5286ff8d58ac039d0c368bb5a654
Carloscv200313 <carlosscv200313@gmail.com>
2025-02-18 23:01:21 -0500

    casi terminando el login

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,15 @@ import { TicTacToe } from './components/TicTacToe';
 
 function App() {
   return (
-    <div className='w-screen h-screen'>
+    <div className='w-screen h-screen overflow-hidden'>
       <div className="relative h-full w-full bg-black">
         <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:32px_32px]">
         </div>
-        <div className='absolute h-full w-full grid grid-cols-2'> 
-          <div className=''>
+        <div className='absolute h-full w-full grid grid-cols-2 overflow-hidden'> 
+          <div className='flex items-center justify-center'>
             <TicTacToe/>
           </div>
-          <div className='flex flex-col items-center justify-center pt-10 rounded-4xl text-white'>
+          <div className='flex flex-col items-center justify-center text-white'>
             <Login />
           </div>
         </div>
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { useNavigate } from "react-router";
+
 export default function Login() {
     const [nombre, setNombre] = useState("");
     const [usuario, setUsuario] = useState("");
     const [password, setPassword] = useState("");
+    const [isSignUp, setIsSignUp] = useState(false);
     const navigate = useNavigate();
 
     const handleSubmit = (e: React.FormEvent) => {
@@ -11,10 +13,8 @@ export default function Login() {
         navigate("/Raya", { state: { nombre, usuario, password } });
     };
 
-    const [isSignUp, setIsSignUp] = useState(false);
-
     return (
-        <div className="relative flex flex-col items-center pt-10 rounded-4xl shadow-xl  bg-black/50">
+        <div className="relative flex flex-col items-center rounded-4xl shadow-xl bg-black/50 pb-8">
             <div className="absolute inset-0 rounded-4xl border-[4px] border-transparent"
                 style={{
                     background: "linear-gradient(-40deg, #e81cff 0%, #40c9ff 100%)",
@@ -22,30 +22,9 @@ export default function Login() {
                     maskComposite: "exclude",
                     WebkitMaskComposite: "destination-out"
                 }}></div>
-            {/* Switch para alternar entre Login y Sign Up */}
-            <div className="relative flex items-center gap-6 mb-0">
-                <span className={`font-semibold text-xl ${!isSignUp ? "text-[#ad9de8]" : ""}`}>Login</span>
-                <label className="relative w-12 h-6 cursor-pointer">
-                    <input
-                        type="checkbox"
-                        className="sr-only"
-                        checked={isSignUp}
-                        onChange={() => setIsSignUp(!isSignUp)}
-                    />
-                    {/* Fondo del switch */}
-                    <span className={`block w-12 h-6 rounded-full transition duration-300 ${!isSignUp ? "bg-[#ad9de8] " : "bg-[#8057d0] "} `}></span>
-                    {/* Bolita deslizante */}
-                    <span
-                        className="absolute top-0.5 left-1 w-5 h-5 bg-white rounded-full shadow-md transition duration-300"
-                        style={{ transform: isSignUp ? "translateX(24px)" : "translateX(0)" }}
-                    ></span>
-                </label>
-                <span className={`font-semibold text-xl ${isSignUp ? "text-[#8057d0] " : ""}`}>Sign up</span>
-            </div>
-
-
-            {/* Contenedor con animación de flip */}
-            <div className="relative w-80 h-96 perspective flex flex-col justify-between items-baseline ">
+            
+            {/* Contenedor de Login/SignUp */}
+            <div className="relative w-80 h-96 perspective flex flex-col justify-between items-baseline">
                 <div
                     className={`relative w-full h-full transition-transform duration-700 transform`}
                     style={{
@@ -55,34 +34,43 @@ export default function Login() {
                 >
                     {/* Lado Login */}
                     <div
-                        className="absolute w-full h-full flex flex-col items-center justify-center  bg-wh rounded-lg shadow-md p-6"
+                        className="absolute w-full h-full flex flex-col items-center justify-center  rounded-lg shadow-md p-6"
                         style={{ backfaceVisibility: "hidden" }}
                     >
-                        <h2 className="text-xl font-bold mb-4">Iniciar Sesion</h2>
-                        <form className="flex flex-col gap-8 w-full justify-between">
+                        <h2 className="text-xl font-bold mb-4">Iniciar Sesión</h2>
+                        <form className="flex flex-col gap-4 w-full">
                             <input type="text" placeholder="Usuario" className="border border-gray-400 p-2 rounded w-full" onChange={(e) => setUsuario(e.target.value)} />
-                            <input type="password" placeholder="Password" className="border border-gray-400 p-2 rounded w-full" onChange={(e) => setPassword(e.target.value)} />
-                            <button className="bg-gray-800 text-white p-2 rounded hover:bg-gray-900">Let's go!</button>
+                            <input type="password" placeholder="Contraseña" className="border border-gray-400 p-2 rounded w-full" onChange={(e) => setPassword(e.target.value)} />
+                            <button className="bg-gray-800 text-white p-2 rounded-4xl hover:bg-gray-900" onClick={handleSubmit}>Ingresar</button>
                         </form>
                     </div>
 
                     {/* Lado Sign Up */}
                     <div
-                        className="absolute w-full h-full flex flex-col items-center justify-center bg-gra0 rounded-lg shadow-md p-6"
+                        className="absolute w-full h-full flex flex-col items-center justify-center  rounded-lg shadow-md p-6"
                         style={{
                             transform: "rotateY(180deg)",
                             backfaceVisibility: "hidden",
                         }}
                     >
-                        <h2 className="text-xl font-bold mb-4">Sign up</h2>
+                        <h2 className="text-xl font-bold mb-4">Crear Cuenta</h2>
                         <form className="flex flex-col gap-4 w-full">
                             <input type="text" placeholder="Nombre" className="border border-gray-400 p-2 rounded w-full" onChange={(e) => setNombre(e.target.value)} />
                             <input type="text" placeholder="Usuario" className="border border-gray-400 p-2 rounded w-full" onChange={(e) => setUsuario(e.target.value)} />
-                            <input type="password" placeholder="Password" className="border border-gray-400 p-2 rounded w-full" onChange={(e) => setPassword(e.target.value)} />
-                            <button className="bg-gray-800 text-white p-2 rounded hover:bg-gray-900">Confirm!</button>
+                            <input type="password" placeholder="Contraseña" className="border border-gray-400 p-2 rounded w-full" onChange={(e) => setPassword(e.target.value)} />
+                            <button className="bg-gray-800 text-white p-2 rounded-4xl hover:bg-gray-900" onClick={handleSubmit}>Registrarse</button>
                         </form>
                     </div>
                 </div>
+            </div>
+            
+            {/* Opción para cambiar entre Login y SignUp */}
+            <div className="relative mt-6 text-sm">
+                {isSignUp ? (
+                    <p className="text-gray-400">¿Ya tienes cuenta? <span className="text-blue-400 cursor-pointer" onClick={() => setIsSignUp(false)}>Inicia sesión</span></p>
+                ) : (
+                    <p className="text-gray-400">¿No tienes cuenta? <span className="text-blue-400 cursor-pointer" onClick={() => setIsSignUp(true)}>Regístrate</span></p>
+                )}
             </div>
         </div>
     );
